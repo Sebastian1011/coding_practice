cfc67ec126a866f408187e208ac59485263c5445
ziming zhang <zzmbpt@163.com>
2019-03-06 01:05:36 +0800

    4

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -40,9 +40,41 @@
 
 
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        i = 0
-        j = 0
-        half = (len(num1) + len(num2) + 1)/2
-        while i + j <= half - 2:
-            return 0.0
+    def findMedianSortedArrays(self, nums1, nums2) -> float:
+        m = len(nums1)
+        n = len(nums2)
+        if m > n:
+            temp = nums1
+            nums1 = nums2
+            nums2 = temp
+            m = len(nums1)
+            n = len(nums2)
+        half = (m + n + 1)//2
+        iMax = m
+        iMin = 0
+        while iMin <= iMax:
+            i = (iMax + iMin)//2
+            j = half - i
+            if i < m and nums2[j - 1] > nums1[i]:
+                iMin = i + 1
+            elif i > 0 and nums1[i - 1] > nums2[j]:
+                iMax = i - 1
+            else:
+                max_left = 0
+                min_right = 0
+                if i == 0:
+                    max_left = nums2[j - 1]
+                elif j == 0:
+                    max_left = nums1[i - 1]
+                else:
+                    max_left = max(nums1[i - 1], nums2[j - 1])
+                if (m + n) % 2 == 1:
+                    return max_left
+                if i == m:
+                    min_right = nums2[j]
+                elif j == n:
+                    min_right = nums1[i]
+                else:
+                    min_right = min(nums1[i], nums2[j])
+
+                return (max_left + min_right) / 2
